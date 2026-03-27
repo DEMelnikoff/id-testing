444398468039bf7a5e2f6cf54278c6f417c3859a
David E Melnikoff <davidemelnikoff@gmail.com>
2026-03-26 20:40:32 -0700

    Update exp.js

--- js/exp.js
+++ js/exp.js
@@ -440,8 +440,8 @@ var exp = (function() {
 
 }());
 
-const timeline = [exp.intro, 
-   // exp.round1_howToPlay, exp.round1_howToEarn, exp.round1, exp.round1_Qs, exp.round1_complete, 
+const timeline = [exp.intro, exp.round1_howToPlay, exp.round1_howToEarn, 
+    // exp.round1, exp.round1_Qs, exp.round1_complete, 
    // exp.round2_howToEarn, exp.round2, exp.round2_Qs, exp.demographics, 
     exp.save_data
 ];
